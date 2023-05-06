77633527ee7afa9ca894184e82d266931d6811b7
vaughn <jwst0208@gmail.com>
2023-05-06 13:07:39 +0900

    feat: crawling basic code

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-### 2023-1 DA Term Project
+# 2023-1 DA Term Project
+
+### Introduction
+
+---
+
+_2023 Spring, Data Analytics (Yoon) Term Project Code._
+
+리뷰 이벤트로 인해 고객이 작성한 리뷰에 대한 신뢰성이 떨어지고 가게의 평점 평균이 실제 값보다 더 높게 표현되는 편향현상이 발생할 수 있다.
+
+본 연구에서는 리뷰 데이터를 크롤링을 이용하여 수집하여 분석하고, 실제로 리뷰이벤트를 진행하는 매장에 편향이 얼마나 크게 발생하는지 분포를 확인할 것이다.
+
+최종적으로는 리뷰 데이터의 특징을 이용하여 해당 리뷰에 대한 신뢰도 추정하는 신경망 모델을 기반으로 가게의 평균 별점에서 편향치를 어느정도 제거한 신뢰성 높은 평점을 제공하는 것을 목적으로 한다.
+
+<br>
+
+### Environment
+
+---
+
+The `requirements.txt` file should list All Python Libraries that your PC depends on. installed by :
+```bash
+pip install -r requirement.txt
+```
+
+<br>
+
+### Run : Crawler
+
+---
+move `src` directory and Using :
+``` bash
+python crawling.py --order [ORDER] --num [NUM] --lat [LAT] --lon [LON]
+```
+_where_ option is 
+
+- order : order criteria
+  - rank
+  - review_avg
+  - review_count
+  - min_order_value
+  - distance
+- num : # review (each)
+- lat : latitude
+- lon : longitude
+
+Basically, more than 2,500 reviews are collected each for stores that hold review events and stores that do not.
+
+@ crawler reference : https://github.com/ariels1996/Yogiyo-Review-Crawling-with-Selenium
---
+++ src/crawling.py
@@ -0,0 +1,310 @@
+import argparse
+
+import time
+import pickle
+import pandas as pd
+from tqdm import trange, tqdm
+import warnings
+
+warnings.filterwarnings("ignore")
+
+import requests
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+# 스크롤 내리기
+def scroll_bottom():
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+
+
+# 1. 해당 카테고리 음식점 리스트 리턴
+def get_restaurant_list(lat, lng, items=100):
+
+    restaurant_list = []
+    # 헤더 선언 및 referer, User-Agent 전송
+    headers = {
+        "referer": "https://www.yogiyo.co.kr/mobile/",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36",
+        "Accept": "application/json",
+        "x-apikey": "iphoneap",
+        "x-apisecret": "fe5183cc3dea12bd0ce299cf110a75a2",
+    }
+    params = {
+        "items": items,
+        "lat": lat,
+        "lng": lng,
+        "order": ORDER_OPTION,
+        "page": 0,
+        "search": "",
+    }
+    host = "https://www.yogiyo.co.kr"
+    path = "/api/v1/restaurants-geo/"
+    url = host + path
+
+    response = requests.get(url, headers=headers, params=params)
+
+    count = 0
+    for item in response.json()["restaurants"]:
+        restaurant_list.append(item["id"])
+        count += 1
+    print(restaurant_list)
+    return list(set(restaurant_list))
+
+
+# 2. 검색한 음식점 페이지 들어가기
+def go_to_restaurant(id):
+    try:
+        restaurant_url = "https://www.yogiyo.co.kr/mobile/#/{}/".format(id)
+        driver.get(url=restaurant_url)
+        print(driver.current_url)
+    except Exception as e:
+        print("go_to_restaurant 에러")
+    time.sleep(5)
+
+
+# 3-1. 해당 음식점의 정보 페이지로 넘어가기
+def go_to_info():
+    print("정보 페이지 로드중...")
+    driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[1]/ul/li[3]/a').click()
+    time.sleep(2)
+    print("정보 페이지 로드 완료")
+
+
+# 3-2. 정보 더보기 클릭하기
+def get_info():
+    op_time = driver.find_element(By.XPATH, '//*[@id="info"]/div[2]/p[1]/span').text
+    addr = driver.find_element(By.XPATH, '//*[@id="info"]/div[2]/p[3]/span').text
+    print(op_time)
+    print(addr)
+    return op_time, addr
+
+
+# 4-1. 해당 음식점의 리뷰 페이지로 넘어가기
+def go_to_review():
+    print("리뷰 페이지 로드중...")
+    driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[1]/ul/li[2]/a').click()
+    time.sleep(2)
+    print("리뷰 페이지 로드 완료")
+
+
+# 4-2. 리뷰 더보기 클릭하기
+def click_more_review():
+    driver.find_element(By.CLASS_NAME, "btn-more").click()
+    time.sleep(2)
+
+
+# 5. 리뷰 페이지 모두 펼치기
+def stretch_review_page():
+    review_count = int(
+        driver.find_element(By.XPATH, '//*[@id="content"]/div[2]/div[1]/ul/li[2]/a/span').text
+    )
+    click_count = int((review_count / 10))
+    print("모든 리뷰 불러오기 시작...")
+    for _ in trange(click_count):
+        try:
+            scroll_bottom()
+            click_more_review()
+        except Exception as e:
+            pass
+    scroll_bottom()
+    print("모든 리뷰 불러오기 완료")
+
+
+# 6. 해당 음식점의 모든 리뷰 객체 리턴
+def get_all_review_elements():
+    try:
+        reviews = driver.find_elements(
+            By.CSS_SELECTOR,
+            "#review > li.list-group-item.star-point.ng-scope"
+        )
+    except Exception as e:
+        print("리뷰 객체 반환 오류")
+        print(e)
+    return reviews
+
+
+# 7. 페이지 뒤로 가기 (한 음식점 리뷰를 모두 모았으면 다시 음식점 리스트 페이지로 돌아감)
+def go_back_page():
+    print("페이지 돌아가기중...")
+    driver.execute_script("window.history.go(-1)")
+    time.sleep(2)
+    print("페이지 돌아가기 완료" + "\n")
+
+
+# 8. 크롤링과 결과 데이터를 pickle 파일과 csv파일로 저장
+def save_pickle_csv(location, yogiyo_df):
+    yogiyo_df.to_csv("C:/Users/CoIn240/PycharmProjects/DA/data/origin/{}_{}_df.csv".format(location[0], location[1]), encoding='utf-8-sig')
+    pickle.dump(yogiyo_df, open("C:/Users/CoIn240/PycharmProjects/DA/data/origin/pkl/{}_{}_df.pkl".format(location[0], location[1]), "wb"))
+    print("{} {} pikcle save complete.".format(location[0], location[1]))
+
+
+# 9. 크롤링 메인 함수
+def yogiyo_crawling(location):
+    # 데이터 프레임 구조 설정
+    df = pd.DataFrame(
+        columns=[
+            "Restaurant",
+            "UserID",
+            "Menu",
+            "Review",
+            "Total",
+            "Taste",
+            "Quantity",
+            "Delivery",
+            "image",
+            "event",
+            "Date",
+            "OperationTime",
+            "Address",
+            
+        ]
+    )
+
+    try:
+        restaurant_list = get_restaurant_list(
+            location[0], location[1], RESTAURANT_COUNT
+        )
+        for restaurant_id in restaurant_list:
+            try:
+                print(
+                    "********** "
+                    + str(restaurant_list.index(restaurant_id) + 1)
+                    + "/"
+                    + str(len(restaurant_list))
+                    + " 번째 **********"
+                )
+
+                go_to_restaurant(restaurant_id)  # 검색한 음식점 클릭
+                go_to_info()  # 음식점 정보창 클릭
+
+                infotext = driver.find_element(By.XPATH, '//*[@id="info"]/div[1]/div[2]').text
+                print(infotext)
+                
+                # 리뷰이벤트 유무 확인
+                rornot ='N'
+
+                if "리뷰이벤트" in infotext:
+                    rornot = "Y"
+                if "리뷰 이벤트" in infotext:
+                    rornot = "Y"
+                if "이벤트" in infotext:
+                    rornot = "Y"
+
+                op_time, addr = get_info()
+
+                go_to_review()
+                stretch_review_page()
+                reviews = get_all_review_elements()
+
+                for review in tqdm(reviews):  # 해당 음식점의 리뷰 수 만큼 데이터를 가져옴
+                    print("please....")
+                    try:
+                        review.find_element(By.CSS_SELECTOR, "table.info-images > tbody > tr > td > div > img")
+                        image = "T"
+                    except:
+                        image = "F"
+
+                    try:
+                        df.loc[len(df)] = {
+                            "Restaurant": driver.find_element(By.CLASS_NAME, "restaurant-name").text,
+                            "UserID": review.find_element(By.CSS_SELECTOR,
+                                                          "span.review-id.ng-binding"
+                                                          ).text,
+                            "Menu": review.find_element(By.CSS_SELECTOR,
+                                                        "div.order-items.default.ng-binding"
+                                                        ).text,
+                            "Review": review.find_element(By.CSS_SELECTOR, "p").text,
+                            "image": image,
+                            "Total": str(
+                                len(
+                                    review.find_elements(By.CSS_SELECTOR,
+                                                        "div > span.total > span.full.ng-scope"
+                                                        )
+                                )
+                            ),
+                            "Taste": review.find_element(By.CSS_SELECTOR,
+                                                         "div:nth-child(2) > div > span.category > span:nth-child(3)"
+                                                         ).text,
+                            "Quantity": review.find_element(By.CSS_SELECTOR,
+                                                            "div:nth-child(2) > div > span.category > span:nth-child(6)"
+                                                            ).text,
+                            "Delivery": review.find_element(By.CSS_SELECTOR,
+                                                            "div:nth-child(2) > div > span.category > span:nth-child(9)"
+                                                            ).text,
+                            "Date": review.find_element(By.CSS_SELECTOR,
+                                                        "div:nth-child(1) > span.review-time.ng-binding"
+                                                        ).text,
+                            "OperationTime": op_time,
+                            "Address": addr,
+                            "event": rornot,
+                        }
+                    except Exception as e:
+                        print("리뷰 페이지 에러")
+                        print(e)
+                        pass
+
+
+            except Exception as e:
+                print("*** 음식점 ID:" + restaurant_id + " *** 음식점 페이지 에러")
+                go_back_page()
+                print(e)
+                pass
+
+            print("음식점 리스트 페이지로 돌아가는중...")
+            go_back_page()
+
+    except Exception as e:
+        print("음식점 리스트 페이지 에러")
+        print(e)
+        pass
+
+    print("End of [ {} - {} ] Crawling.".format(location[0], location[1]))
+    save_pickle_csv(location, df)
+    print("{} {} crawling finish.".format(location[0], location[1]))
+
+    return df
+
+
+# 10. 요기요 크롤링 실행 함수
+def start_yogiyo_crawling():
+
+    locations = [[LAT, LON]]
+
+    for location in locations:
+        try:
+            yogiyo = yogiyo_crawling(location)
+        except Exception as e:
+            print(e)
+            pass
+
+# 실행 환경
+parser = argparse.ArgumentParser(description="Arguments for Crawler")
+parser.add_argument(
+    "--order",
+    required=False,
+    default="review_count",
+    help="option for restaurant list order / choose one \
+    -> [rank, review_avg, review_count, min_order_value, distance, estimated_delivery_time]",
+)
+parser.add_argument("--num", required=False, default=100, help="option for amount of review data")
+parser.add_argument("--lat", required=False, default=37.4573, help="latitude for search")
+parser.add_argument("--lon", required=False, default=126.9506, help="longitude for search")
+args = parser.parse_args()
+
+ORDER_OPTION = args.order
+RESTAURANT_COUNT = int(args.num)
+LAT = float(args.lat)
+LON = float(args.lon)
+
+# 크롬 드라이버 경로 설정
+chromedriver = "C:/Users/CoIn240/PycharmProjects/DA/Application/chrome.exe"
+driver = webdriver.Chrome(chromedriver)
+
+url = "https://www.yogiyo.co.kr/mobile/#/"
+
+# fake_useragent 모듈을 통한 User-Agent 정보 생성
+driver.get(url=url)
+print(driver.current_url)
+
+start_yogiyo_crawling()
