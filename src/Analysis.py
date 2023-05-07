a80473a3659b000b961624b77d83bb92e62fe614
webb-c <jwst0208@gmail.com>
2023-05-07 16:36:23 +0900

    feat: add Analysis code (init)

---
+++ src/Analysis.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from nltk import FreqDist
+from wordcloud import WordCloud
+from collections import Counter
+
+# file = '../data/dataset'
+file_Y = '../data/test/노원구_전처리_Y.csv'
+file_N = '../data/test/노원구_전처리_N.csv'
+
+
